e5db7fc7023fe8d60ab7a4e9732326b4bbfac94e
Javier Selle <jselle@primerosystems.com>
2021-01-28 12:36:11 -0300

    Web url changed.

--- API/Startup.cs
+++ API/Startup.cs
@@ -23,7 +23,7 @@ namespace API
             services.AddScoped<App>();
             services.AddCors(opt => {
                 opt.AddPolicy("CorsPolicy", policy => {
-                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000", "https://ahorcado-web-v2.herokuapp.com");
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000", "https://ahorcado-web-v3-1.herokuapp.com/");
                 });
             });
         }
